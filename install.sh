183caf75fdf1c6bec871792846b3534fc0cd1904
phaelicks <105660748+phaelicks@users.noreply.github.com>
2023-06-28 15:16:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 python3 -m pip install -r requirements.txt
-cd abides
-python3 -m pip install -r requirements.txt
-python3 -m pip install -r requirements-dev.txt
-cd abides-core
+cd abides/abides-core
 python3 setup.py install
 cd ../abides-markets
 python3 setup.py install
