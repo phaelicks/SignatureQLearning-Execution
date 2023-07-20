8e611493636fd0b17eed9d400dc0abcd9cc84852
phaelicks <105660748+phaelicks@users.noreply.github.com>
2023-07-20 12:47:38 +0200

    Small updates

--- src/train_v1.py
+++ src/train_v1.py
@@ -58,42 +58,38 @@ def train(
         episode_inventory = []
 
         observation = env.reset()[:,0] # as row vector, gym 0.18.0 returns only state at reset
-        action = 10 # do nothing
-        initial_tuple = np.hstack((observation, action / 10))
+        action = 2 # do nothing
         episode_actions.append(action)
 
         history = deque(maxlen=window_length)
-        history.append(initial_tuple)
+        history.append(observation)
 
-        initial_tuple_tensor = torch.tensor(
-            [initial_tuple], requires_grad=False, dtype=torch.float
+        observation_tensor = torch.tensor(
+            [observation], requires_grad=False, dtype=torch.float
         )
 
         # initialize signature variable
-        history_signature = policy.update_signature(initial_tuple_tensor.unsqueeze(0))
-        last_tuple_tensor = initial_tuple_tensor
+        history_signature = policy.update_signature(observation_tensor.unsqueeze(0))
+        last_observation_tensor = observation_tensor
 
-        # first step outside of loop
         done = False
-        observation, reward, done, _ = env.step(action)
-        observation = observation[:,0]
+        step_counter = 0
 
         # RUN EPISODE
-        step_counter = 0
         while not done:
             if step_counter < do_nothing_steps:
                 # agent only observes 
-                action = 10 # next action is 'do nothing'
-                new_tuple = np.hstack((observation, action / 10))
-                history.append(new_tuple)
+                action = 2 # next action is 'do nothing'
                 episode_actions.append(action)
 
                 observation, _, _, _ = env.step(action)
                 observation = observation[:,0]
-                step_counter += 1
-                last_tuple_tensor = torch.tensor(
-                    [new_tuple], requires_grad=False, dtype=torch.float
+                last_observation_tensor = torch.tensor(
+                    [observation], requires_grad=False, dtype=torch.float
                 )
+
+                history.append(observation)
+                step_counter += 1
 
                 continue
 
@@ -104,39 +100,13 @@ def train(
                 )                
             
             # create Q values and select action
-            Q = policy.create_Q_values(history_signature, last_tuple_tensor, observation)
+            Q = policy(history_signature)
             if np.random.rand(1) < epsilon:
                 action = np.random.randint(0, env.action_space.n-1)
             else:
                 _, action = torch.max(Q, -1)
                 action = action.item()
             episode_actions.append(action)
-
-            """
-            new_t = np.hstack((observation, action))
-            new_t_t = torch.tensor([new_t], requires_grad = False, dtype=torch.float)
-            new_p = torch.cat((last_tuple_tensor, new_t_t), 0).unsqueeze(0)
-            signature_1 = policy.update_signature(new_p, last_tuple_tensor, history_signature)
-            Q_selected = policy(signature_1)[0][0]
-            """
-
-            # add (o,a) tuple to history
-            next_tuple = np.hstack((observation, action / 10))
-            next_tuple_tensor = torch.tensor(
-                [next_tuple], requires_grad=False, dtype=torch.float
-            )                     
-
-            # update history and signature
-            history.append(next_tuple) # pops left if maxlen != None               
-            if window_length == None:
-                new_path = torch.cat((next_tuple_tensor, last_tuple_tensor), 0).unsqueeze(0)
-                history_signature = policy.update_signature(new_path, last_tuple_tensor, history_signature)
-            else: 
-                history_signature = policy.update_signature(
-                    torch.tensor(history, requires_grad=False, dtype=torch.float).unsqueeze(0)
-                )
-            #print(history_signature)
-            # TODO: find way to compute signature of shortened path via Chen
 
             # take action
             observation, reward, done, info = env.step(action)
@@ -145,20 +115,36 @@ def train(
                 print("reward: {} | pnl: {} | inv reward: {} | inventory {}".format(
                     reward, info["pnl"], info["inventory_reward"], info["inventory"]
                 )
+            )    
+            episode_inventory.append(info["inventory"])                
+
+            # update history and signature
+            history.append(observation) # pops left if maxlen != None
+            observation_tensor = torch.tensor(
+                    [observation], requires_grad=False, dtype=torch.float
             )
 
-            episode_inventory.append(info["inventory"])
+            if window_length == None:
+                new_path = torch.cat((last_observation_tensor, observation_tensor), 0).unsqueeze(0)
+                history_signature = policy.update_signature(
+                    new_path, last_observation_tensor, history_signature
+                )
+            else: 
+                history_signature = policy.update_signature(
+                    torch.tensor(history, requires_grad=False, dtype=torch.float).unsqueeze(0)
+                )
+            #print(history_signature)
+            # TODO: find way to compute signature of shortened path via Chen
 
             Q_target = torch.tensor(reward, requires_grad=False, dtype=torch.float)            
             if not done:
-                Q1 = policy.create_Q_values(history_signature, next_tuple_tensor, observation)
+                Q1 = policy(history_signature)
                 maxQ1, _ = torch.max(Q1, -1)
                 Q_target += torch.mul(maxQ1, discount)
             Q_target.detach_()
             
             #print(Q[action])
             loss = loss_fn(Q[action], Q_target)
-            #loss = loss_fn(Q_selected, Q_target)
             if printing:
                 print("loss:", loss)
             policy.zero_grad()
@@ -181,7 +167,7 @@ def train(
                 cash_history.append(info["cash"])
                 terminal_inventory.append(info["inventory"])
             else:
-                last_tuple_tensor = next_tuple_tensor
+                last_observation_tensor = observation_tensor
             
             if done or step_counter % 200 == 0:
                 print(
@@ -191,9 +177,7 @@ def train(
                 )
                 print("Q values:", Q)
 
-        # take steps
-        #scheduler.step()
-        #epsilon = initial_epsilon * epsilon_decay(episode)
+        env.close()
 
         # Record history
         loss_history.append(episode_loss)
@@ -204,16 +188,14 @@ def train(
         #action_history.append(episode_actions)
         #inventory_history.append(episode_inventory)
 
-        env.close()
+        if (episode+1) % 10 == 0:
+            policy_copy = deepcopy(policy.state_dict())
+            intermediate_policies.append(policy_copy)
 
         if (episode+1) % 1000 == 0:
             optimizer = optim.Adam(policy.parameters(), lr=learning_rate)
             scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=learning_rate_decay)
             #scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.999)
-
-        if (episode+1) % 10 == 0:
-            policy_copy = deepcopy(policy.state_dict())
-            intermediate_policies.append(policy_copy)
 
     results = {
         "rewards": reward_history,
